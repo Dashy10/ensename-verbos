e44d9c923c77d0aef48e43cf3adbc23437088082
Dashy10 <dashiell.lumas@gmail.com>
2017-04-29 15:44:11 -0400

    heroku

--- db/queries.js
+++ db/queries.js
@@ -2,3 +2,27 @@ var pgp = require('pg-promise')();
 var connString = process.env.DATABASE_URL;
 // connects to our database
 var db = pgp(connString);
+
+
+function getAllverbs(req, res, next) {
+  console.log('inside function getAllverbs',req.body)
+  // ALLOWS YOU TO TYPE IN ANY COMMAND THAT HAS MORE THAN ONE COMPONENT
+  db.any('select * from verbs_present')
+    .then(function(data) {
+      console.log('inside data verbs_present',data)
+      console.log('DATA:', data);
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'All Verbs'
+        });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
+
+module.exports = {
+  getAllverbs: getAllverbs
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../db/queries');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Spanish Verb Conjugator' });
 });
+
+router.get('/api/spanish_verbs', db.getAllverbs);
 
 module.exports = router;
