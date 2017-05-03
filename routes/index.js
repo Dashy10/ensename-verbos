b4856c4b930ea3753828aa432fa72e8bcbe97873
Dashy10 <dashiell.lumas@gmail.com>
2017-05-03 18:59:46 -0400

    ui basic skeleton

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,11 @@ var db = require('../db/queries');
 /* GET home page. */
 // RENDERING INDEX.VIEW FILE
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Spanish Verb Conjugator' });
+  res.render('index', { title: 'Enseñame Tus Verbos' });
+});
+
+router.get('/contribuciones', function(req, res, next) {
+  res.render('contribuciones', {title: 'Contribucíones'});
 });
 // DYNAMICALLY CREATING NEW ROUTE THAT INVOKES GETALLVERBS FUNCTION
 
