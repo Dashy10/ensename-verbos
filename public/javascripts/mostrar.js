947590b96ddc7dc4e68f2d81d6d1e18e96f379ba
Dashy10 <dashiell.lumas@gmail.com>
2017-05-06 13:13:03 -0400

    mvp done: front-end GET working and Full CRUD working, working on front-end Update, Delete and Post

--- public/javascripts/mostrar.js
+++ public/javascripts/mostrar.js
@@ -8,6 +8,7 @@
 
   $('#get_verbs').on('click',function(e){
     e.preventDefault();
+    $('.mostrame').css("opacity",".7");
     let input = $('#verb_input').val();
 
     let verb = encodeURI(input);
@@ -15,23 +16,54 @@
     axios.get(present_tenseURL + verb)
     .then(function(res){
       // console.log(res.data.data)
-      displayVerb(res.data.data.group)
-      // window.location.href ='/present-tense/' + verb
+      displayYo(res.data.data.yo)
+      displayTu(res.data.data.tú)
+      displayThird(res.data.data.third)
+      displayNosotros(res.data.data.nosotros)
+      displayVosotros(res.data.data.vosotros)
+      displayGroup(res.data.data.group)
     });
   });
 
-displayVerb = (allverbs) => {
+displayYo = (yo) => {
   // ul = $("<ul/>");
-  let all_present_tense_verbs = allverbs
+  let all_present_tense_verbs = yo
   console.log(all_present_tense_verbs)
-  $(".mostrame").append(allverbs);
-  
-  // $(all_present_tense_verbs).each(function(){
-  //   $("<li/>").text(this).appendTo(ul);
-  // });
+  $("#singular-first").empty().append(yo);
+}
+displayTu = (tu) => {
+  // ul = $("<ul/>");
+  let all_present_tense_verbs = tu
+  console.log(all_present_tense_verbs)
+  $("#singular-second").empty().append(tu);
+}
 
+displayThird = (third) => {
+  // ul = $("<ul/>");
+  let all_present_tense_verbs = third
+  console.log(all_present_tense_verbs)
+  $("#singular-third").empty().append(third);
+}
 
+displayNosotros = (nosotros) => {
+  // ul = $("<ul/>");
+  let all_present_tense_verbs = nosotros
+  console.log(all_present_tense_verbs)
+  $("#plural-first").empty().append(nosotros);
+}
 
+displayVosotros = (vosotros) => {
+  // ul = $("<ul/>");
+  let all_present_tense_verbs = vosotros
+  console.log(all_present_tense_verbs)
+  $("#plural-second").empty().append(vosotros);
+}
+
+displayGroup = (group) => {
+  // ul = $("<ul/>");
+  let all_present_tense_verbs = group
+  console.log(all_present_tense_verbs)
+  $("#plural-third").empty().append(group);
 }
 
 // $('#get_verbs').on('click',function(){
